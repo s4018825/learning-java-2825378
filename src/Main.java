a712a835af653a0737c94d8b84ab32d5bb82e5de
Tommy <tran_tommy@outlook.com>
2026-02-09 22:02:32 +1100

    Working with primitive data types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int tommyAge = 25;
+        double tommyGPA = 0.9;
+        char tommyInitial = 'T';
+        char tommyLastInitial = 'T';
+        Boolean hasPerfectAttendance = false;
+
+        System.out.println(tommyAge);
+        System.out.println(tommyGPA);
+        System.out.println(tommyInitial);
+        System.out.println(tommyLastInitial);
+        System.out.println(hasPerfectAttendance);
     }
 }
